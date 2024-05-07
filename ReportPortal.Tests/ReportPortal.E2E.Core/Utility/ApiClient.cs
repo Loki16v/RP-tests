0cfd2c5ccebe69de31b9b9c0057236c1478cfe11
Ivan Dorosevich <idorosevich@lenovo.com>
2024-05-07 12:19:03 +0400

    Resolve comments

--- ReportPortal.Tests/ReportPortal.E2E.Core/Utility/ApiClient.cs
+++ ReportPortal.Tests/ReportPortal.E2E.Core/Utility/ApiClient.cs
@@ -10,12 +10,20 @@ namespace ReportPortal.E2E.Core.Utility
         public static IHttpClient Get(string baseUrl, AuthorizationMessageHandler authorizationMessageHandler = null)
         {
             var clientName = TestsBootstrap.Instance.Configuration.GetSection("ApiClient").GetValueOrThrow();
-            return clientName switch
+            Enum.TryParse(clientName, out Client apiClient);
+            return apiClient switch
             {
-                "HttpClient" => authorizationMessageHandler == null ? new HttpClient(baseUrl) : new HttpClient(baseUrl, authorizationMessageHandler),
-                "RestClient" => authorizationMessageHandler == null ? new RestClient(baseUrl) : new RestClient(baseUrl, authorizationMessageHandler),
-                _ => throw new Exception($"Available api clients are '' or '' instead of '{clientName}'")
+                Client.HttpClient => authorizationMessageHandler == null ? new HttpClient(baseUrl) : new HttpClient(baseUrl, authorizationMessageHandler),
+                Client.RestClient => authorizationMessageHandler == null ? new RestClient(baseUrl) : new RestClient(baseUrl, authorizationMessageHandler),
+                _ => throw new Exception($"Available api clients are '{Client.RestClient}' or '{Client.HttpClient}' instead of '{clientName}'")
             };
+        }
+
+        private enum Client
+        {
+            None = 0,
+            HttpClient,
+            RestClient
         }
     }
 }
